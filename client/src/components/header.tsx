0d567247027570f02496c6fae1c7a1bff849540e
Eugene Zhang <zhang.eug@gmail.com>
2024-01-13 17:10:24 -0500

    Add webpack img bundling

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import styled from "styled-components";
+import plantLogo from "../../public/images/plant.png";
 
 const Header: React.FC = () => {
-  return <div>header component</div>;
+  return (
+    <HeaderContainer>
+      <LogoImg src={plantLogo} alt="Leaf logo" />
+      header component
+    </HeaderContainer>
+  );
 };
+
+const HeaderContainer = styled.div`
+  background: #9fdc9f;
+  height: 18px;
+`;
+
+const LogoImg = styled.img`
+  width: 13px;
+  height: 10px;
+`;
 
 export default Header;
