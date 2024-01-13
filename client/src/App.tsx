572d0805ccb82ee178d6aba37c8e2d1e90879250
Eugene Zhang <zhang.eug@gmail.com>
2024-01-13 14:42:17 -0500

    Clean up files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      Hello World
-    </div>
-  );
-}
+const App: React.FC = () => {
+  return <div className="App">Hello World</div>;
+};
 
 export default App;
