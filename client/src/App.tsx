f58730ab6a55bc1d042ca7852d29fe3970380cd4
Eugene Zhang <zhang.eug@gmail.com>
2024-01-13 16:55:07 -0500

    Fix files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import "./App.css";
-import styled from "styled-components";
 import Webcam from "react-webcam";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import GrowthStunters from "./components/growth-stunters";
 
 const App: React.FC = () => {
   return (
     <>
-      Hello World
+      <Header />
+      <GrowthStunters />
+      <Footer />
       <Webcam width={150} />
     </>
   );
