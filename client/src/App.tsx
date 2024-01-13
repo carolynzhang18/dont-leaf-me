207fbe6b41146394af73b696e9b05be2e89026d8
Carolyn Zhang <carolyn.zhang18@gmail.com>
2024-01-13 16:29:10 -0500

    Fix merge conflicts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import "./App.css";
 import styled from "styled-components";
+import Webcam from "react-webcam";
 
 const App: React.FC = () => {
-  return <AppContainer className="App">Hello World</AppContainer>;
+  return (<AppContainer className="App">
+      Hello World
+      <Webcam width={150} />
+    </AppContainer>);
 };
 
 const AppContainer = styled.div`
