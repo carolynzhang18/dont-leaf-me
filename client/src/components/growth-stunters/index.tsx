10c249246f481dd2fb1d1659057c8c331f4f5ab0
Jessica <jessica2003mail@gmail.com>
2024-01-13 17:51:40 -0500

    add newURL form

--- client/src/components/growth-stunters/index.tsx
+++ client/src/components/growth-stunters/index.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 const GrowthStunters: React.FC = () => {
-  return <div>hello world</div>;
+  let urls = ["https://www.youtube.com/"];
+  const [url, setUrl ] = useState('');
+
+  const submit = () => {
+    urls.push(url);
+    console.log("urls", urls);
+  }
+
+  return (
+    <>
+      <form>
+        <label>
+          Add new URL :
+          <input type="text" name="name" onChange={(e) => setUrl(e.target.value)} />
+        </label>
+      </form>
+      <button type="submit" onClick={submit}>Submit</button>
+    </>
+  );
 };
 
 export default GrowthStunters;
