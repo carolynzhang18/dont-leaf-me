f24726774e1bf9a2d6621629b76f25aa79acb932
Carolyn Zhang <carolyn.zhang18@gmail.com>
2024-01-13 16:56:52 -0500

    Add backend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import "./App.css";
-import Webcam from "react-webcam";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import GrowthStunters from "./components/growth-stunters";
+import FaceDetector from "./components/FaceDetector";
 
 const App: React.FC = () => {
   return (
     <>
       <Header />
+      <FaceDetector />
       <GrowthStunters />
       <Footer />
-      <Webcam width={150} />
     </>
   );
 };
